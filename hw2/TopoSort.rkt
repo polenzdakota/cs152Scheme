ee6eff921dc189f9faab95c60c54dad92a0f001b
Matt Pleva <pleva.matt@gmail.com>
2015-03-12 18:56:23 -0700

    Renamed file so that we can submit it. Needs comments

--- hw2/TopoSort.rkt
+++ hw2/TopoSort.rkt
@@ -1,5 +1,10 @@
-(define graph '((1 2 3 4) (2 4 5) (3 6) (4 3 6 7) (5 4 7) (6) (7 6)))
-(define graph2 graph)
+; Authors Matt Pleva, Donovon Bacon, Daktoa Polenz
+; Cs 152
+; 2015-03-11
+; Homework 2
+
+(define graph '((1 2 3 4) (2 4 5) (3 6) (4 3 6 7) (5 4 7) (6) (7 6) (8 3)))
+(define graph2 '((1 3) (3 2) (2 3 4) (4 5) (5 3)))
 
 (define (createListWithoutElement l e)
   (if (equal? l '()) '()
@@ -24,7 +29,8 @@
 
 (define (removeNodeFromGraph node graphList)
   (if (equal? graphList '()) '()
-    (if (equal? (caar graphList) node) '()
+    (if (equal? (caar graphList) node)
+      (removeNodeFromGraph node (cdr graphList))
       (cons (car graphList) (removeNodeFromGraph node (cdr graphList)))
     )
   )
@@ -32,7 +38,7 @@
 
 (define (topoSort l)
   (if (equal? l '()) '()
-    (append (findZeroIndegree l (buildList l))
+    (append (list (car(findZeroIndegree l (buildList l))))
       (topoSort
         (removeNodeFromGraph
           (if (equal? (findZeroIndegree l (buildList l)) '()) '()
@@ -49,3 +55,4 @@
 (findZeroIndegree graph (buildList graph))
 (removeNodeFromGraph 1 graph)
 (topoSort graph)
+(topoSort graph2)
